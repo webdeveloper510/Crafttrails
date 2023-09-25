aba2ca5c651db5177d6df9cc1a88611483461c3a
webdeveloper510 <webdeveloper510@gmail.com>
2023-09-25 10:37:37 +0530

    first commit

---
+++ Craftapp/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from Craftapp.manager import CustomUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+
+class AbstractTimestampedModel(models.Model):
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+   class Meta:
+      abstract = True
+      
+
+
+class User(AbstractBaseUser,PermissionsMixin,AbstractTimestampedModel):
+    first_name  = models.CharField(max_length=30,default="")
+    last_name  = models.CharField(max_length=30,default="")
+    email 		= models.EmailField(_('email'),unique=True)
+    password    = models.CharField(max_length=255,default="")
+    is_staff 	= models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.')
+    is_active 	= models.BooleanField(default=True,
+		help_text='Designates whether this user should be treated as active.\
+		Unselect this instead of deleting accounts.')
+    brewery=models.IntegerField(blank=True,null=True)
+
+    USERNAME_FIELD 	='email'
+    
+    objects 		= CustomUserManager()
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
