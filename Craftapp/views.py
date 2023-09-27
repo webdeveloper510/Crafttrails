bf73c6afe1cd17e67e11b485bb2a2e2b07296e28
webdeveloper510 <webdeveloper510@gmail.com>
2023-09-27 16:13:21 +0530

    changes

--- Craftapp/views.py
+++ Craftapp/views.py
@@ -70,18 +70,53 @@ class BreweriesView(APIView):
             }
             app_ids = settings.breweries_id 
             response = requests.post(f"{base_url}/{app_ids}/records/list/", headers=headers, json={"hydrated": True})
+         
             for i in response.json()["items"]:
                 data={
                     "application_id":i["id"],
                     "title":i["title"],
                     "home_city":i["scc71d5fd5"],
                     "home_name":i["sb1594021e"],
-                    "bar_name":i["s8a95871e9"]
+                    "bar_name":i["s8a95871e9"],
+                    "max_capacity":i["s954ab521d"],
+                    "title_submenu":{
+                        "title":i["title"],
+                        "trail_link":i["sf6l341b"],
+                        "indore_seating":i["s143e1cdf6"],
+                        "outdoor_seting":i["s99c9760cf"],
+                        "kid_friendly":i["sc43d290a1"],
+                        "dog_friendly":i["sbfdbacabe"],
+                        "brewery_name":i["s4092fdc16"],
+                        "contact_name":i["s1bf5c94ab"],
+                        "contact_email":i["s621deff9c"],
+                        "reach_number":i["s0941b34fd"],
+                        "customer_face_phone":i["s4f68aacef"],
+                        "address":i["s6bc0e1667"],
+                        "have_live_music":i["sbb661fe53"],
+                        "food_available":i["s827ace380"],
+                        "public_tour":i["s57a08fb4b"],
+                        "untappd_url":i["s9yt9ss7"],
+                        "twitter_url":i["s8ykzlvc"],
+                        "instagram_url":i["szm9u2od"],
+                        "facebook_url":i["s586c43040"],
+                        "formula":i["s809a2b9cb"],
+                        "event_calender":i["s8e5b5d9d9"],
+                        "food_photos":i["s44be84238"],
+                        "photo_of_drink":i["s12dd86f04"],
+                        "drink_option":i["s6a53b58cb"],
+                        "website":i["s4a2531de8"],
+                        "establishment":i["sfb39beff9"],
+                        "logo":i["sa24832ad2"],
+                        
+                    
+                     
+                    }
                 }
                 breweries_list.append(data)  
             data=breweries_list
-            return Response({"code":200,"data":data},status=status.HTTP_200_OK)
+            return Response({"code":200,"data":breweries_list},status=status.HTTP_200_OK)
         except Exception as e:
+            print(e)
             return Response({"code":400,"error":"Unable to fetch data"},status=status.HTTP_200_OK)
 
 
