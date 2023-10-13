0740430c23dc67949bc4f1ffb78d5129b85829a9
Priyanka <developerweb6@gmail.com>
2023-10-13 17:50:35 +0530

    hashRouter

--- src/app/routing/AppRoutes.tsx
+++ src/app/routing/AppRoutes.tsx
@@ -6,7 +6,7 @@
  */
 
 import { FC } from 'react'
-import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Navigate, HashRouter } from 'react-router-dom'
 import { PrivateRoutes } from './PrivateRoutes'
 import { ErrorsPage } from '../modules/errors/ErrorsPage'
 import { Logout, AuthPage, useAuth } from '../modules/auth'
@@ -21,7 +21,7 @@ const { PUBLIC_URL } = process.env
 const AppRoutes: FC = () => {
   const { auth } = useAuth()
   return (
-    <BrowserRouter basename={PUBLIC_URL}>
+    <HashRouter basename={PUBLIC_URL}>
       <Routes>
         <Route element={<App />}>
           <Route path='error/*' element={<ErrorsPage />} />
@@ -43,7 +43,7 @@ const AppRoutes: FC = () => {
           <Route path="terms-of-services" element={<TermsAndServices />} /> */}
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
