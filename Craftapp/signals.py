3977157ad541d7a059947031e20dc10f035ed1c2
webdeveloper510 <webdeveloper510@gmail.com>
2023-10-20 16:55:16 +0530

    hottest day

--- Craftapp/signals.py
+++ Craftapp/signals.py
@@ -10,12 +10,14 @@ from django_rest_passwordreset.signals import reset_password_token_created
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
     
     val2=instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm'))
+   
     val=reset_password_token.key
+    amin_value="http://localhost:3000/#/auth/reset-password/?token="+reset_password_token.key
     context = {
         'current_user': reset_password_token.user,
         'username': reset_password_token.user.first_name,
         'email': reset_password_token.user.email,
-        'reset_password_url': val2 +"?token="+val,
+        'reset_password_url':amin_value,
     }
 
     email_html_message = render_to_string('emails/password_reset_email.html', context)
