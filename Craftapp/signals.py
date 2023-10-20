8cea9321877c75448bc2a547366aff9bbae6f5ef
webdeveloper510 <webdeveloper510@gmail.com>
2023-10-20 16:58:55 +0530

    hottest day

--- Craftapp/signals.py
+++ Craftapp/signals.py
@@ -12,12 +12,12 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     val2=instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm'))
    
     val=reset_password_token.key
-    amin_value="http://localhost:3000/#/auth/reset-password/?token="+reset_password_token.key
+    main_value="http://localhost:3000/#/auth/reset-password/?token="+reset_password_token.key
     context = {
         'current_user': reset_password_token.user,
         'username': reset_password_token.user.first_name,
         'email': reset_password_token.user.email,
-        'reset_password_url':amin_value,
+        'reset_password_url':main_value,
     }
 
     email_html_message = render_to_string('emails/password_reset_email.html', context)
