e94e52367346ffe5c17c85c1a5abc2a891587ea1
webdeveloper510 <developerweb6@gmail.com>
2023-11-02 10:23:07 +0530

    changes done

--- CraftTrails/settings.py
+++ CraftTrails/settings.py
@@ -120,12 +120,14 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://localhost:3000',
     'https://trailmetrics.cctrails.com'
   
     
 ]
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
+    'https://localhost:3000',
     'https://trailmetrics.cctrails.com'
 ]
 
