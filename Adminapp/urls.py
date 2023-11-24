ddec8d9576d30c9688edfb6f8691b668e5cd1b54
webdeveloper510 <developerweb6@gmail.com>
2023-11-24 12:35:31 +0530

    changes done

--- Adminapp/urls.py
+++ Adminapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("user/status/<int:id>",ActIactView.as_view(),name="userstatus",),
     path("user/edit/<int:id>",UserEditView.as_view(),name="useredit"),
     path("user/location/<int:id>",ChangeLocationView.as_view(),name="userlocation"),
-    path("user/update/<int:id>",ChangeProfileView.as_view(),"userupdate")
+    path("user/update/<int:id>",ChangeProfileView.as_view(),name="userupdate")
 ]
