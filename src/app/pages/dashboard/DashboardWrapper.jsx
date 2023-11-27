256f22b9cdaac5b5bdbe91215054954c45efb54b
Priyanka <developerweb6@gmail.com>
2023-11-27 11:32:05 +0530

    font change

--- src/app/pages/dashboard/DashboardWrapper.jsx
+++ src/app/pages/dashboard/DashboardWrapper.jsx
@@ -65,7 +65,7 @@ const DashboardPage = () => {
       ) : (
         <>
           <div className="row">
-            <span className="display-6 mb-5 pb-4 text-center">{name}</span>
+            <h1 className="display-6 mb-5 pb-4 text-center">{name}</h1>
           </div>
           <div className="row g-5 g-xl-10 mb-5 mb-xl-10">
             <div className="col-md-6 col-lg-6 col-xl-6 col-xxl-6 mb-md-5 mb-xl-10">
